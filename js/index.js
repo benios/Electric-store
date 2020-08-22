83dde754cf52016eaf541529d4448e43e55b158b
yosziad <Yosziad88@gmail.com>
2020-08-22 17:30:40 +0300

    final commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
+function closeWindow(){ 
+    document.querySelector(".signin").style.visibility = "hidden"; 
+}
+
 // closing window
-    document.querySelector(".signin").addEventListener("click",function (){ 
-        document.querySelector(".signin").style.visibility = "hidden"; 
-    })
+    document.querySelector(".cross").addEventListener("click", closeWindow)
+
+// validating username and password
+    function validateCheck(){
+        var userValidationRegEx=/^[a-z]|[0-9]|-|_+$/;
+        var passwordElement=document.getElementById('userPassword');
+        var userElement=document.getElementById('user');
+        var userName = userElement.value;
+        var passwordValue=passwordElement.value;
+      
+        if(userValidationRegEx.test(userName) && (passwordElement.value).length>=8) {
+            closeWindow();
+            alert("connected successfully");
+        }
+        else{
+            alert("Error!!! the highlighted fields are incorrect");
+            if(!userValidationRegEx.test(userName)){
+                userElement.style.borderColor = "green";
+                userElement.style.borderWidth= "0.3rem"  
+            }
+            if((passwordElement.value).length<8){
+                passwordElement.style.borderColor = "green";
+                passwordElement.style.borderWidth= "0.3rem" 
+            }
+        }
+        return false;
+    }
