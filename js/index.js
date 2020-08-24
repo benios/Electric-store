468d7e2b75259ed4a745ae029c00a5cb4d4224b1
yosziad <Yosziad88@gmail.com>
2020-08-24 20:15:11 +0300

    cr fix

--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
-function closeWindow(){ 
-    document.querySelector(".signin").style.visibility = "hidden"; 
+function closeWindow() {
+  document.querySelector(".signin").style.visibility = "hidden";
 }
 
 // closing window
-    document.querySelector(".cross").addEventListener("click", closeWindow)
+document.querySelector(".cross").addEventListener("click", closeWindow);
 
 // validating username and password
-    function validateCheck(){
-        var userValidationRegEx=/^[a-z]|[0-9]|-|_+$/;
-        var passwordElement=document.getElementById('userPassword');
-        var userElement=document.getElementById('user');
-        var userName = userElement.value;
-        var passwordValue=passwordElement.value;
-      
-        if(userValidationRegEx.test(userName) && (passwordElement.value).length>=8) {
-            closeWindow();
-            alert("connected successfully");
-        }
-        else{
-            alert("Error!!! the highlighted fields are incorrect");
-            if(!userValidationRegEx.test(userName)){
-                userElement.style.borderColor = "green";
-                userElement.style.borderWidth= "0.3rem"  
-            }
-            if((passwordElement.value).length<8){
-                passwordElement.style.borderColor = "green";
-                passwordElement.style.borderWidth= "0.3rem" 
-            }
-        }
-        return false;
+function validateCheck() {
+  var userValidationRegEx = /^([a-z]|[0-9]|-|_)+$/;
+  var passwordElement = document.getElementById("userPassword");
+  var userElement = document.getElementById("user");
+  var userName = userElement.value;
+  var passwordValue = passwordElement.value;
+
+  if (userValidationRegEx.test(userName) && passwordValue.length >= 8) {
+    closeWindow();
+    alert("connected successfully");
+  } else {
+    alert("Error!!! the highlighted fields are incorrect");
+    if (!userValidationRegEx.test(userName)) {
+      userElement.style.borderColor = "red";
+      userElement.style.borderWidth = "0.3rem";
     }
+    if (passwordValue.length < 8) {
+      passwordElement.style.borderColor = "red";
+      passwordElement.style.borderWidth = "0.3rem";
+    }
+  }
+  return false;
+}
