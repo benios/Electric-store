7c38b84c630a87317fc7d5d4e5814ce77fbdc68a
yosziad <Yosziad88@gmail.com>
2020-09-04 11:59:35 +0300

    adding bootstrap validation

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
+
+
 function closeWindow() {
 	document.querySelector(".signin").style.visibility = "hidden";
 }
-
 // closing window
 document.querySelector(".cross").addEventListener("click", closeWindow);
-
 // validating username and password
 function validateCheck() {
 	var userValidationRegEx = /^([a-z]|[0-9]|-|_)+$/;
@@ -14,16 +14,16 @@ function validateCheck() {
 	var passwordValue = passwordElement.value;
 	var isUsernameValid = userValidationRegEx.test(userName);
 	if (isUsernameValid && passwordValue.length >= 8) {
-		closeWindow();
-		alert("connected successfully");
+    userElement.classList.add("is-valid");
+    passwordElement.classList.add("is-valid");
+    setTimeout(closeWindow(),9500000);
 	} else {
-		alert("Error!!! the highlighted fields are incorrect");
 		if (!isUsernameValid) {
-			userElement.classList.add("invalid");
+			userElement.classList.add("is-invalid");
 		}
 		if (passwordValue.length < 8) {
-			passwordElement.classList.add("invalid");
+			passwordElement.classList.add("is-invalid");
 		}
 	}
 	return false;
-}
+ }
