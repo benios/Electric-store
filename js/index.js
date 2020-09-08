5b16526cdecfd2acfc9c0574e672d8670c2b6e26
yosziad <Yosziad88@gmail.com>
2020-09-08 17:21:31 +0300

    Adding bootstrap fixed

--- js/index.js
+++ js/index.js
@@ -1,30 +1,47 @@
 
-
-function closeWindow() {
-	document.querySelector(".signin").style.visibility = "hidden";
-}
-// closing window
-document.querySelector(".cross").addEventListener("click", closeWindow);
-
 // validating username and password
 function validateCheck() {
+
 	var userValidationRegEx = /^([a-z]|[0-9]|-|_)+$/;
 	var passwordElement = document.getElementById("userPassword");
 	var userElement = document.getElementById("user");
 	var userName = userElement.value;
 	var passwordValue = passwordElement.value;
-	var isUsernameValid = userValidationRegEx.test(userName);
-	if (isUsernameValid && passwordValue.length >= 8) {
-    userElement.classList.add("is-valid");
-    passwordElement.classList.add("is-valid");
-    setTimeout(closeWindow(),9500000);
-	} else {
-		if (!isUsernameValid) {
-			userElement.classList.add("is-invalid");
-		}
-		if (passwordValue.length < 8) {
-			passwordElement.classList.add("is-invalid");
-		}
-	}
+  var isUsernameValid = userValidationRegEx.test(userName);
+  if(isUsernameValid && passwordValue.length >= 8){
+    $('#myModal').modal('hide');
+  }
+  else{
+    if (isUsernameValid) {
+      userElement.classList.add("is-valid");
+      userElement.classList.remove("is-invalid");
+    } 
+    else {
+        userElement.classList.add("is-invalid");
+        userElement.classList.remove("is-valid");
+      }
+      if(passwordValue.length >= 8){
+        passwordElement.classList.add("is-valid");
+        passwordElement.classList.remove("is-invalid");
+       }
+      else{
+        passwordElement.classList.add("is-invalid");
+        passwordElement.classList.remove("is-valid");
+      }
+  }
 	return false;
  }
+ 
+
+//  clearing the connectiion window
+
+function clearWindow(){
+  var passwordElement =document.getElementById("userPassword");
+  var userElement=document.getElementById("user");
+  passwordElement.value = '';
+  userElement.value = '';
+  userElement.classList.remove("is-invalid");
+  userElement.classList.remove("is-valid");
+  passwordElement.classList.remove("is-invalid");
+  passwordElement.classList.remove("is-valid");
+}
